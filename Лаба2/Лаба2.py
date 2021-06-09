15f1ae072f14f8cba2054e3a136dfbed779ff9cb
Animeshka39 <sergiosasa9@gmail.com>
2021-06-09 17:13:44 +0300

    4

--- Лаба2/Лаба2.py
+++ Лаба2/Лаба2.py
@@ -1,93 +1,78 @@
 
-from tkinter import *
 import os
- 
- 
-def dir():
-    qwe2 = ['']
-    qwe = os.listdir(os.getcwd())
-    sith = len(qwe)
-    i=0
-    while(i<sith):
-        qwe2.append(qwe[i])
-        i+=1
-    return(qwe2)
+from tkinter import *
+from tkinter import messagebox
+import time
+import pyautogui as pag
 
-def back():
-    os.chdir("..")
-    listb()
 
-def open_f():
-    selection = languages_listbox.curselection()
-    a = languages_listbox.get(selection[0])
-    for j in range(len(a)):
-        if a[j] == ".":
-            os.startfile(os.getcwd()  + "\\" + a)
-            listb()
-            return()
-    os.chdir(os.getcwd()  + "\\" + a)
-    listb()
-def go():
-    folder = language_entry.get()
-    os.chdir(folder )
-    listb()
+clicks = 0
 
-def create_f():
-    new_f = language_entry.get()
-    open(new_f, "w")
-    listb()
-def create_p():
-    new_p = language_entry.get()
-    os.mkdir(new_p)
-    listb()
-def del_f():
-    selection = languages_listbox.curselection()
-    new_d = languages_listbox.get(selection[0])
-    for j in range(len(new_d)):
-        if new_d[j] == ".":
-            os.remove(new_d)
-            listb()
-            return()
-
-    os.rmdir(new_d)
-    listb()
-def renam():
-    selection = languages_listbox.curselection()
-    new_bif = languages_listbox.get(selection[0])
-    new_aft = language_entry.get()
-
-    os.rename(new_bif, new_aft)
-    listb()
-def repl():
-    selection = languages_listbox.curselection()
-    new_bif = languages_listbox.get(selection[0])
-    new_aft = language_entry.get()
-
-    os.replace(new_bif, new_aft)
-    listb()
- 
 root = Tk()
-root.title("Comander")
+root.title("GUI на Python")
+root.geometry("377x200")
 root['bg'] = '#555'
- 
-language_entry = Entry(width=35)
-language_entry.grid(column=0, row=0, padx=6, pady=6)
-create_f_button = Button(text="Створ.Файл",background="#555", foreground="#ccc", command=create_f).grid(column=2, row=0, padx=6, pady=6)
-create_p_button = Button(text="Створ.Папку",background="#555", foreground="#ccc", command=create_p).grid(column=1, row=0, padx=6, pady=6)
- 
-languages_listbox = Listbox(height=20, width=50)
-languages_listbox.grid(row=1, column=0, columnspan=2,rowspan=20, sticky=W+E, padx=5, pady=5)
- 
-def listb():
-    languages_listbox.delete(0,'end')
-    for language in dir():
-        languages_listbox.insert(END, language) 
 
-back_button = Button(text="Назад",background="#555", foreground="#ccc", command=back, width=8).grid(row=3, column=2, padx=5, pady=5)
-open_button = Button(text="Открить",background="#555", foreground="#ccc", command=open_f, width=8).grid(row=4, column=2, padx=5, pady=5)
-go_button = Button(text="Перейти",background="#555", foreground="#ccc", command=go, width=8).grid(row=2, column=2, padx=5, pady=5)
-del_button = Button(text="Удалить",background="#555", foreground="#ccc", command=del_f, width=8).grid(row=5, column=2, padx=5, pady=5)
-renam_button = Button(text="Переімен.",background="#555", foreground="#ccc", command=renam, width=8).grid(row=6, column=2, padx=5, pady=5)
-repl_button = Button(text="Переміст.",background="#555", foreground="#ccc", command=repl, width=8).grid(row=7, column=2, padx=5, pady=5)
-listb()
+type = IntVar()
+timeout = StringVar()
+
+Label1 = Label(text="Enter timeout value:", background="#555", foreground="#ccc")
+Label1.pack()
+Label1.place(x=0,y=60)
+
+timeout_entry = Entry(textvariable=timeout)
+timeout_entry.place(x=140,y=60)
+
+hour_checkbutton = Radiobutton(text = "Hours",background="#555", foreground="#ccc", value=1, variable=type)
+hour_checkbutton.pack()
+hour_checkbutton.place(x=0, y=90)
+
+minute_checkbutton = Radiobutton(text = "Minutes",background="#555", foreground="#ccc", value=2, variable=type)
+minute_checkbutton.pack()
+minute_checkbutton.place(x=0, y=120)
+
+seconds_checkbutton = Radiobutton(text = "Seconds",background="#555", foreground="#ccc", value=3, variable=type)
+seconds_checkbutton.pack()
+seconds_checkbutton.place(x=0, y=150)
+ 
+def click_button():
+    global clicks
+    clicks += os.system("shutdown /s /t " + str(timeout.get()))
+def hibernation():
+    global clicks
+    os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+def reboot():
+    global clicks
+    clicks += os.system("shutdown /r /t " + str(timeout.get()))
+def close_windows():
+    global clicks
+    clicks += os.system("%windir%\explorer.exe shell:::{3080F90D-D7AD-11D9-BD98-0000947B0257}" + str(timeout.get()))
+def open_windows():
+    global clicks
+    pag.hotkey('win','home',interval=str(timeout.get()))
+ 
+
+ 
+btn = Button(text="Shutdown", background="#555", foreground="#ccc",
+             padx="20", pady="8", font="16", command=click_button)
+btn.place(x=0, y=0)
+
+btn1 = Button(text="Hibernation", background="#555", foreground="#ccc",
+             padx="20", pady="8", font="16", command=hibernation)
+
+btn1.place(x=130, y=0)
+
+btn2 = Button(text="Reboot", background="#555", foreground="#ccc",
+             padx="20", pady="8", font="16", command=reboot)
+btn2.place(x=275, y=0)
+
+btn3 = Button(text="Close windows", background="#555", foreground="#ccc",
+             padx="20", pady="8", font="16", command=close_windows)
+btn3.place(x=200, y=90)
+
+btn4 = Button(text="Open windows", background="#555", foreground="#ccc",
+             padx="20", pady="8", font="16", command=open_windows)
+btn4.place(x=200, y=145)
+
 root.mainloop()
+
