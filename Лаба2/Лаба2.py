59b4db801b4a7bbed18636fd5c89b36b0825e553
Animeshka39 <sergiosasa9@gmail.com>
2021-06-13 21:00:49 +0300

    Update Лаба2.py

--- Лаба2/Лаба2.py
+++ Лаба2/Лаба2.py
@@ -1,83 +1,106 @@
-import math
-
+import os
 while True:
-    menu = int(input(  "Ведіть номер задачі від 1 до 10 або введіть: 999 для заверщення роботи "))
-    if menu == 1:
-        x = input(  "x =  ")
-        y = input(  "y =  ")
+    error = 0
+    menufail = 0
+    os.system('dir')
+    menu = int(input("1-Назад \n2-Перейти в \n3-Змінити диск \n4-Створити папку \n5-Видалити папку \n6-Переіменувати \n7-Операції з файлами \n9-Вихід\n"))
+    if menu == 7: menufail = int(input("1-Відкрити файл \n2-Створити \n3-Запис тексту в файл \n4-Перемістити \n5-Видалити \n9-Вихід\n" ))
+    if menu == 1: os.chdir("..")
+    if menu == 2: 
+        a = str(input("Назва папки \n"))
+        os.chdir(os.getcwd()  + "\\" + a)
+    if menu == 3: 
+        while error == 0:
+            a = str(input("Вкажіть диск (для відміни введіть 9) \n"))
+            try: 
+                os.chdir(a + ":")
+                error = 1
+            except FileNotFoundError or OSError:
+               print("Такого диску не існує виберіть інший \n")
+               error = 0
+            if a == "": 
+                error = 1
 
-        s = float(x) + float(y)
-        r = float(x) - float(y)
-        d = float(x) * float(y)
-
-        prfloat ("Сума " + str(s) + "\n")
-        prfloat ("Рiзниця " + str(r) + "\n")
-        prfloat ("Добуток " + str(d) + "\n")
-
-    if menu == 2:
-        x = input(  "x =  ")
-        y = input(  "y =  ")
-        s = (math.fabs(float(x)) - math.fabs(float(y))) / (1 + math.fabs(float(x) * float(y)))
-        prfloat ("Результат "  + str(s) + "\n")
-
-    if menu == 3:
-        x = input(  "Довжина ребра куба =  ")
-        o = float(x) * float(x) * float(x) 
-        s = float(x) * float(x)
-        print ("Обєм куба " + str(o) + "\n")
-        print ("Площа бокової поверхностi " + str(s) + "\n")
-
-    if menu == 4:
-        x = input(  "x =  ")
-        y = input(  "y =  ")
-        s = (float(x) + float(y)) / 2
-        p = math.sqrt(float(x) * float(y) )
-        print ("Сер. ариф. " + str(s) + "\n")
-        print ("Сер. геом. "  + str(p) + "\n")
-
-    if menu == 5:
-        x = input(  "x =  ")
-        y = input(  "y =  ")
-        s = (float(x) + float(y)) / 2
-        p = math.sqrt(math.fabs(float(x)) * math.fabs(float(y)) )
-        print ("Сер. ариф. " + str(s) + "\n")
-        print ("Сер. геом. "  + str(p) + "\n")
-
+    if menu == 4: 
+        a = str(input("Вкажіть назву папки \n"))
+        os.mkdir(a)
+    if menu == 5: 
+        while error == 0:
+            a = str(input("Вкажіть назву папки (для відміни введіть 99exit)\n"))
+            try: 
+                os.rmdir(a)
+                error = 1
+            except FileNotFoundError:
+               print("Такої папки не існує ")
+               error = 0
+            if a == "99exit": error = 1
     if menu == 6:
-        x = input(  "Катет а = ")
-        y = input(  "Катет б = ")
-        s = math.sqrt((float(x) * float(x)) + (float(y) * float(y)))
-        p = float(x) + float(y)
-        print ("Гiпотенуза "  + str(s) + "\n")
-        print ("Площа "  + str(p) + "\n")
+        while error == 0:
+            a = str(input("Вкажіть назву файла або папки яку бажаєте переіменувати (для відміни введіть 99exit)\n"))
+            try: 
+                b = str(input("Вкажіть як бажаєте назвати \n"))
+                os.rename(a, b)
+                error = 1
+            except FileNotFoundError:
+                print("Такого файла не існує \n")
+                error = 0
+            if a == "99exit": error = 1
 
-    if menu == 7:
-        o1 = input(  "Обєм 1 = ")
-        o2 = input(  "Обєм 2 = ")
-        t1 = input(  "Температура 1 = ")
-        t2 = input(  "Температура 2 = ")
-        oz = float(o1) * float(o2) 
-        tz = (float(o1) * float(t1) + float(o2) * float(t2)) / oz
-        print ("Загальний обєм = "  + str(oz) + "\n")
-        print ("Загальна температура = "  + str(tz) + "\n")
+    if menufail == 1:
+        while error == 0:
+            a = str(input("Вкажіть назву файла (для відміни введіть 99exit)\n"))
+            try: 
+                os.startfile(a)
+                error = 1
+            except FileNotFoundError:
+               print("Такого файла не існує \n")
+               error = 0
+            if a == "99exit": error = 1
+    if menufail == 2:
+            a = str(input("Вкажіть назву файла (для відміни введіть 99exit)\n"))
+            if a == "99exit":continue
+            open(a, "w")
 
+    if menufail == 3:
+        while error == 0:
+            a = str(input("Вкажіть назву файла в який бажаєте ввести текст (для відміни введіть 99exit)\n"))
+            b = str(input("Вкажіть як текст бажаєте записати \n"))
+            try: 
+                text_file = open(a, "a")
+                text_file.write(b)
+                text_file.close()
+                error = 1
+            except FileNotFoundError:
+                print("Такого файла не існує ")
+                error = 0
+            if a == "99exit": error = 1
 
-    if menu == 8:
-        r = input(  "Радiус =  ")
-        n = input(  "Кiлькiсть сторiн = ")
-        p = 2 * float(r) * float(n) * math.sin(math.pi / float(n))* math.cos(math.pi / float(n))
-        print ("Периметр = "  + str(p) + "\n")
+    if menufail == 4:
+        while error == 0:
+            a = str(input("Вкажіть назву файла який бажаєте перемістити (для відміни введіть 99exit)\n"))
+            b = str(input("Вкажіть шлях переміщення "))
+            try: 
+                os.replace(a, b)
+                error = 1
+            except FileNotFoundError:
+               print("Такого файла не існує ")
+               error = 0
+            if a == "99exit": error = 1
+    if menufail == 5:
+        while error == 0:
+            a = str(input("Вкажіть назву файла який бажаєте видалити (для відміни введіть 99exit)\n"))
+            try: 
+                os.remove(a)
+                error = 1
+            except FileNotFoundError:
+               print("Такого файла не існує ")
+               error = 0
+            if a == "99exit": error = 1
 
-    if menu == 9:
-        r1 = input(  "Опiр 1 =  ")
-        r2 = input(  "Опiр 2 =  ")
-        r3 = input(  "Опiр 3 =  ")
-        t = 1/float(r1) + 1/float(r2) + 1/float(r3)
-        print ("Заг. опiр = "  + str(t) + "\n")
-
-    if menu == 10:
-        h = input(  "Висота = ")
-        t = math.sqrt((2 * float(h)) / 9.81)
-        print ("Час = "   + str(t) + "\n")
-    if menu == 999:
+            
+    os.system('cls ')    
+    if menu == 9 or menufail == 9: 
         break
+
+
+
